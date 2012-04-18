938f3a0578ce3a1a69d3dd3016931bfb2ecc8871
Mark Grant <merely.semantics@gmail.com>
2012-04-18 11:22:56 -0500

    Section two

--- src/clojurekoans/view/index.hiccup.clj
+++ src/clojurekoans/view/index.hiccup.clj
@@ -3,11 +3,46 @@
  [:div#inner-wrapper
   [:div.section
 
-   [:img#bull1 {:align "right" :src "images/bull1.gif"}
+   [:img#bull1 {:align "right" :src "images/bulls/bull1.gif"}
     [:h1 "Introduction"]
 
     [:p "Clojure koans are exercises meant to initiate you to the mysteries of the Clojure. By following along the path set before you, you will touch on all"
      " the major aspects of the language, from simple datatypes to macros, from tail recursion to Java interoperability. Many will find Clojure quite different"
      " from any programming language they've used before. The koans' incremental, interactive approach to learning the language will help make the transition"
      " feel natural. By the time you are finished with them, Clojure will not be so mysterious at all."]
-     ]]]]
+    ]]
+
+  [:div.section
+   [:img#bull2 {:align "right" :src "images/bulls/bull2.gif"}
+    [:h1 "Install Java"]
+
+    [:p " Before we begin the koans, we need Clojure, and before we install Clojure, we'll need Java. You probably already have Java installed, but"
+     "if you don't, it's not difficult. Oracle provides installation instructions for Windows users"
+     [:a {:href "http://www.java.com/en/download/help/windows_manual_download.xml"} "here"]
+     "and for Linux users"
+     [:a href {"http://www.java.com/en/download/help/linux_install.xml"} "here"]
+     ". Apple distributes Java 6 with their system update tool."]
+    [:br]
+    [:p "After downloading Java (if necessary), go to your console and enter:"]
+    [:div.terminal "~ $ java -version"]
+    [:p "Make sure the version installed is 1.5 or greater."]]]
+
+  [:div.section
+   [:img#bull3 {:align "right" :src "images/bulls/bull3.gif"}
+    [:h1 "Introduction"]
+
+    [:p "Clojure koans are exercises meant to initiate you to the mysteries of the Clojure. By following along the path set before you, you will touch on all"
+     " the major aspects of the language, from simple datatypes to macros, from tail recursion to Java interoperability. Many will find Clojure quite different"
+     " from any programming language they've used before. The koans' incremental, interactive approach to learning the language will help make the transition"
+     " feel natural. By the time you are finished with them, Clojure will not be so mysterious at all."]
+    ]]
+
+  [:div.section
+   [:img#bull1 {:align "right" :src "images/bulls/bull1.gif"}
+    [:h1 "Introduction"]
+
+    [:p "Clojure koans are exercises meant to initiate you to the mysteries of the Clojure. By following along the path set before you, you will touch on all"
+     " the major aspects of the language, from simple datatypes to macros, from tail recursion to Java interoperability. Many will find Clojure quite different"
+     " from any programming language they've used before. The koans' incremental, interactive approach to learning the language will help make the transition"
+     " feel natural. By the time you are finished with them, Clojure will not be so mysterious at all."]
+    ]]]]]]]

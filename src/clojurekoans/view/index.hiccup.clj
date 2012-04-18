1f6034b5bdd677be291c60b35cdfea8bbbfc00bc
Mark Grant <merely.semantics@gmail.com>
2012-04-18 11:08:23 -0500

    First section in hiccup

--- src/clojurekoans/view/index.hiccup.clj
+++ src/clojurekoans/view/index.hiccup.clj
@@ -1,1 +1,13 @@
-[:div "Welcome to Clojure Koans"]
+
+[:div {:id "outer-wrapper"}
+ [:div#inner-wrapper
+  [:div.section
+
+   [:img#bull1 {:align "right" src="images/bull1.gif"}
+    [:h1 "Introduction"]
+
+    [:p "Clojure koans are exercises meant to initiate you to the mysteries of the Clojure. By following along the path set before you, you will touch on all"
+     " the major aspects of the language, from simple datatypes to macros, from tail recursion to Java interoperability. Many will find Clojure quite different"
+     " from any programming language they've used before. The koans' incremental, interactive approach to learning the language will help make the transition"
+     " feel natural. By the time you are finished with them, Clojure will not be so mysterious at all."]
+     ]]]]
--- src/clojurekoans/view/layout.hiccup.clj
+++ src/clojurekoans/view/layout.hiccup.clj
@@ -3,7 +3,7 @@
  [:head
   [:meta {:http-equiv "Content-Type" :content "text/html" :charset "iso-8859-1"}]
   [:title "clojurekoans"]
-  (include-css "/stylesheets/clojurekoans.css")
+  (include-css "/stylesheets/style.css")
   (include-js "/javascript/clojurekoans.js")]
  [:body
   (eval (:template-body joodo.views/*view-context*))

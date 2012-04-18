065cf238318ed4a589a4ccd53a3972602e315266
Mark Grant <merely.semantics@gmail.com>
2012-04-18 12:12:06 -0500

    Added jQuery

--- src/clojurekoans/view/layout.hiccup.clj
+++ src/clojurekoans/view/layout.hiccup.clj
@@ -4,6 +4,7 @@
   [:meta {:http-equiv "Content-Type" :content "text/html" :charset "iso-8859-1"}]
   [:title "clojurekoans"]
   (include-css "/stylesheets/style.css")
+  (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
   (include-js "/javascript/clojurekoans.js")]
  [:body
   (eval (:template-body joodo.views/*view-context*))

54a7beabb9d9a87454717a9d5269c912c4b0337e
Mark Grant <merely.semantics@gmail.com>
2012-04-25 14:15:43 -0500

    Added clojure.data.json to normal dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clojurekoans "0.0.1"
   :description "A website deployable to AppEngine"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [joodo "0.6.0-SNAPSHOT"]]
-  :dev-dependencies [[speclj "1.4.0"]
-                     [org.clojure/data.json "0.1.2"]]
+                 [joodo "0.6.0-SNAPSHOT"]
+                 [org.clojure/data.json "0.1.2"]]
+  :dev-dependencies [[speclj "1.4.0"]]
   :test-path "spec/"
   :java-source-path "src/"
   :repl-init-script "config/development/repl_init.clj"

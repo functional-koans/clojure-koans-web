882ace29a9b815838129aecbc1f8fea3e0324e02
Mark Grant <merely.semantics@gmail.com>
2012-04-27 13:39:22 -0500

    Cross-browser style changes

--- src/clojurekoans/view/layout.hiccup.clj
+++ src/clojurekoans/view/layout.hiccup.clj
@@ -3,6 +3,10 @@
  [:head
   [:meta {:http-equiv "Content-Type" :content "text/html" :charset "iso-8859-1"}]
   [:title "Clojure Koans"]
+
+  "<--[if IE]>"
+  (include-css "/stylesheets/iestyle.css")
+  "<![endif]-->"
   (include-css "/stylesheets/style.css")
   (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
   (include-js "/javascript/clojurekoans.js")]

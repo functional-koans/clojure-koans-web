3f827c13ea82e20b3819d28a3d378ffbaa146a15
Mark Grant <merely.semantics@gmail.com>
2012-04-27 14:06:49 -0500

    Conditional CSS change

--- src/clojurekoans/view/layout.hiccup.clj
+++ src/clojurekoans/view/layout.hiccup.clj
@@ -4,11 +4,9 @@
   [:meta {:http-equiv "Content-Type" :content "text/html" :charset "iso-8859-1"}]
   [:title "Clojure Koans"]
 
+  (include-css "/stylesheets/style.css")
   "<!--[if IE]>"
   (include-css "/stylesheets/iestyle.css")
-  "<![endif]-->"
-  "<!--[if !IE]>"
-  (include-css "/stylesheets/style.css")
   "<![endif]-->"
   (include-js "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
   (include-js "/javascript/clojurekoans.js")]
